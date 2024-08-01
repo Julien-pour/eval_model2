5ece4a093f5634ebbf320dee9622c04d9c765f91
Julien Pourcel <j.pourcel31830@gmail.com>
2024-08-01 11:04:33 +0200

    add diff fitness

--- script/launch_script_train_test_on_archive.py
+++ script/launch_script_train_test_on_archive.py
@@ -47,7 +47,7 @@ MAXWAIT=40
 sleep $((RANDOM % MAXWAIT))
 
 conda activate vllm532
-python inference_vllm.py --base_path $WORK/eval_model2/ -e {e} -b 4 --arg_gpu "v100" -a 2 --lr={lr} --test_base_model {test_base_model} --arg_bs_test 1024 --arg_model_id {model_id_2} --seed {seed} --path_archive "archives/{name_archive}" --n_gpu {n_gpu_inference} --path_archive_test "archives/test_archive.json" --file_save_name "cross_archive_test"
+python inference_vllm.py --base_path $WORK/eval_model2/ -e {e} -b 4 --arg_gpu "v100" -a 2 --lr={lr} --test_base_model {test_base_model} --arg_bs_test 1024 --arg_model_id {model_id_2} --seed {seed} --path_archive "archives/{name_archive}" --n_gpu {n_gpu_inference} --path_archive_test "archives/{test_archive}" --file_save_name "cross_archive_test"
 """
 
 if not os.path.exists('slurm/slurm_files'):
@@ -56,67 +56,68 @@ if not os.path.exists('slurm/slurm_files'):
 model_id = "deepseek-coder-1.3b-instruct"#"Meta-Llama-3-8B-Instruct"
 # testset_archive="preprocess_p3_emb_dedup_puzzles.json"
 for id_model,model_id in enumerate(["deepseek-coder-1.3b-instruct","Meta-Llama-3-8B-Instruct"]):
-    if id_model==0:
-        script_2="""
-    python finetune.py --base_path $WORK/eval_model2/ --path_archive "archives/{name_archive}" -e {e} -b 4 --arg_gpu "v100" -a 2 --lr={lr} --test_base_model {test_base_model} --arg_model_id {model_id}
-    conda deactivate
-    module purge 
-    module load python/3.11.5
-    MAXWAIT=40
-    sleep $((RANDOM % MAXWAIT))
+    for path_test_archive in ["test_archive_diff_50.json","test_archive_diff_75.json","test_archive_diff_85.json","test_archive_diff_80.json"]:
+        if id_model==0:
+            script_2="""
+        python finetune.py --base_path $WORK/eval_model2/ --path_archive "archives/{name_archive}" -e {e} -b 4 --arg_gpu "v100" -a 2 --lr={lr} --test_base_model {test_base_model} --arg_model_id {model_id}
+        conda deactivate
+        module purge 
+        module load python/3.11.5
+        MAXWAIT=40
+        sleep $((RANDOM % MAXWAIT))
 
-    conda activate vllm532
-    python inference_vllm.py --base_path $WORK/eval_model2/ -e {e} -b 4 --arg_gpu "v100" -a 2 --lr={lr} --test_base_model {test_base_model} --arg_bs_test 1024 --arg_model_id {model_id_2} --seed {seed} --path_archive "archives/{name_archive}" --n_gpu {n_gpu_inference} --path_archive_test "archives/test_archive.json" --file_save_name "cross_archive_test"
-    """
-    else:
-        script_2="""
-    python finetune.py --base_path $WORK/eval_model2/ --path_archive "archives/{name_archive}" -e {e} -b 1 --arg_gpu "v100" -a 8 --lr={lr} --test_base_model {test_base_model} --arg_model_id {model_id}
-    conda deactivate
-    module purge 
-    module load python/3.11.5
-    MAXWAIT=40
-    sleep $((RANDOM % MAXWAIT))
+        conda activate vllm532
+        python inference_vllm.py --base_path $WORK/eval_model2/ -e {e} -b 4 --arg_gpu "v100" -a 2 --lr={lr} --test_base_model {test_base_model} --arg_bs_test 1024 --arg_model_id {model_id_2} --seed {seed} --path_archive "archives/{name_archive}" --n_gpu {n_gpu_inference} --path_archive_test "archives/{test_archive}" --file_save_name "cross_archive_test"
+        """
+        else:
+            script_2="""
+        python finetune.py --base_path $WORK/eval_model2/ --path_archive "archives/{name_archive}" -e {e} -b 1 --arg_gpu "v100" -a 8 --lr={lr} --test_base_model {test_base_model} --arg_model_id {model_id}
+        conda deactivate
+        module purge 
+        module load python/3.11.5
+        MAXWAIT=40
+        sleep $((RANDOM % MAXWAIT))
 
-    conda activate vllm532
-    python inference_vllm.py --base_path $WORK/eval_model2/ -e {e} -b 1 --arg_gpu "v100" -a 8 --lr={lr} --test_base_model {test_base_model} --arg_bs_test 1024 --arg_model_id {model_id_2} --seed {seed} --path_archive "archives/{name_archive}" --n_gpu {n_gpu_inference} --path_archive_test "archives/test_archive.json" --file_save_name "cross_archive_test"
-    """
+        conda activate vllm532
+        python inference_vllm.py --base_path $WORK/eval_model2/ -e {e} -b 1 --arg_gpu "v100" -a 8 --lr={lr} --test_base_model {test_base_model} --arg_bs_test 1024 --arg_model_id {model_id_2} --seed {seed} --path_archive "archives/{name_archive}" --n_gpu {n_gpu_inference} --path_archive_test "archives/{test_archive}" --file_save_name "cross_archive_test"
+        """
 
-    e=1
-    n_gpu=4
-    n_gpu_train=2
-    test_base_model="False"
-    dev_script=""#"#SBATCH --qos=qos_gpu-dev"
-    list_archive=[]
+        e=1
+        n_gpu=4
+        n_gpu_train=2
+        test_base_model="False"
+        dev_script=""#"#SBATCH --qos=qos_gpu-dev"
+        list_archive=[]
 
-    list_name = ["rd_gen","aces_elm"]
-    list_archive = [i+".json" for i in list_name]
-    # data_order= "random"
-    e=1
-    lr=1e-6
-    # for e in [1]:
-    #     for lr in [1e-6]:
+        list_name = ["rd_gen","aces_elm"]
+        list_archive = [i+".json" for i in list_name]
+        # data_order= "random"
+        e=1
+        lr=1e-6
+        # for e in [1]:
+        #     for lr in [1e-6]:
 
-    list_archive=[]
-    list_seed = [5]
-    for name in list_name:
-        for seed in list_seed:
-            archive = name+"_seed-"+str(seed)+".json"
-            # name_archive=archive.split(".json")[0]
-            script_formated = script_1.format(n_gpu=n_gpu,dev_script=dev_script,h="2")+script_2.format(name_archive=archive,e=e,lr=lr,test_base_model=test_base_model,model_id=model_id,model_id_2=model_id,seed=seed,n_gpu_inference=n_gpu)
-            extra_path='lr-mode'+model_id[:5]+str(lr)+'-e-'+str(e)
-            slurmfile_path = f'slurm/run_v100'+extra_path+'.slurm'
-            with open(slurmfile_path, 'w') as f:
-                f.write(script_formated)
-                # print(script_formated)
-            subprocess.call(f'sbatch {slurmfile_path}', shell=True)
+        list_archive=[]
+        list_seed = [5]
+        for name in list_name:
+            for seed in list_seed:
+                archive = name+"_seed-"+str(seed)+".json"
+                # name_archive=archive.split(".json")[0]
+                script_formated = script_1.format(n_gpu=n_gpu,dev_script=dev_script,h="2")+script_2.format(name_archive=archive,e=e,lr=lr,test_base_model=test_base_model,model_id=model_id,model_id_2=model_id,seed=seed,n_gpu_inference=n_gpu,test_archive=path_test_archive)
+                extra_path='lr-mode'+model_id[:5]+str(lr)+'-e-'+str(e)
+                slurmfile_path = f'slurm/run_v100'+extra_path+'.slurm'
+                with open(slurmfile_path, 'w') as f:
+                    f.write(script_formated)
+                    # print(script_formated)
+                subprocess.call(f'sbatch {slurmfile_path}', shell=True)
 
-    name="wizard_gen"
-    seed=1
-    archive = name+"_seed-"+str(seed)+".json"
-    script_formated = script_1.format(n_gpu=n_gpu,dev_script=dev_script,h="2")+script_2.format(name_archive=archive,e=e,lr=lr,test_base_model=test_base_model,model_id=model_id,model_id_2=model_id,seed=seed,n_gpu_inference=n_gpu)
-    extra_path='lr-mode'+model_id[:5]+str(lr)+'-e-'+str(e)
-    slurmfile_path = f'slurm/run_v100'+extra_path+'.slurm'
-    with open(slurmfile_path, 'w') as f:
-        f.write(script_formated)
-        # print(script_formated)
-    subprocess.call(f'sbatch {slurmfile_path}', shell=True)
+        name="wizard_gen"
+        seed=1
+        archive = name+"_seed-"+str(seed)+".json"
+        script_formated = script_1.format(n_gpu=n_gpu,dev_script=dev_script,h="2")+script_2.format(name_archive=archive,e=e,lr=lr,test_base_model=test_base_model,model_id=model_id,model_id_2=model_id,seed=seed,n_gpu_inference=n_gpu,test_archive=path_test_archive)
+        extra_path='lr-mode'+model_id[:5]+str(lr)+'-e-'+str(e)
+        slurmfile_path = f'slurm/run_v100'+extra_path+'.slurm'
+        with open(slurmfile_path, 'w') as f:
+            f.write(script_formated)
+            # print(script_formated)
+        subprocess.call(f'sbatch {slurmfile_path}', shell=True)
